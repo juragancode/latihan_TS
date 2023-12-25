07506c7284bbd3766164ce455ac2b666144343af
petanicode <syabilkahfialfatih@gmail.com>
2023-12-25 18:31:42 +0800

    union types

--- app.js
+++ app.js
@@ -1,45 +1,20 @@
-// const person : 
-// {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [boolean, number, string];
-//     cita: string;
-// } 
-// = {
-//     name: 'Muhammad Rusdan Habibi',
-//     age: 8,
-//     hobbies: ['Main Bola', 'Playing Gitar'],
-//     role: [false, 1, 'two'],
-//     cita: 'Pemain Bola Profesional seperti Messi',
+// function combine(input1: number, input2: number) {
+//   const result = input1 + input2;
+//   return result;
 // }
-var Role;
-(function (Role) {
-    Role[Role["Admin"] = 0] = "Admin";
-    Role[Role["Super_Admin"] = 1] = "Super_Admin";
-    Role[Role["Author"] = 2] = "Author";
-    Role[Role["Read_Only"] = 3] = "Read_Only";
-    Role[Role["Free_Access"] = 4] = "Free_Access";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Muhammad Rusdan Habibi',
-    age: 8,
-    hobbies: ['Main Bola', 'Playing Gitar'],
-    role: Role.Admin,
-    cita: 'Pemain Bola Profesional seperti Messi',
-};
-var favoriteActivities;
-favoriteActivities = ['Main Bola'];
-// let favoriteActivities : any[];
-// favoriteActivities = ['Main Bola', 1];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+// const combineAges = combine(30, 26);
+// console.log(combineAges);
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-console.log(person.age);
-console.log(person.hobbies[0]);
-console.log(person.hobbies[1]);
-console.log(person.cita);
-console.log(person.role);
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combineNames = combine("Max", "Anna");
+console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,5 +1,23 @@
-function tambah(n1: number, n2: number, ) {
-  
-    const result = n1 + n2;
-  return result
+// function combine(input1: number, input2: number) {
+//   const result = input1 + input2;
+//   return result;
+// }
+
+// const combineAges = combine(30, 26);
+// console.log(combineAges);
+
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
+
+const combineAges = combine(30, 26);
+console.log(combineAges);
+
+const combineNames = combine("Max", "Anna");
+console.log(combineNames);
