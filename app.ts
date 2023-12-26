7f611041802c941edc993b90d83d80483b0a3ef8
petanicode <syabilkahfialfatih@gmail.com>
2023-12-26 13:34:22 +0800

    Function Return Types & void

--- app.ts
+++ app.ts
@@ -1,43 +1,14 @@
-// function combine(input1: number, input2: number) {
-//   const result = input1 + input2;
-//   return result;
-// }
-
-// const combineAges = combine(30, 26);
-// console.log(combineAges);
-
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  return result;
-
-  // if (resultConversion === "as-number") {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 26, "as-number");
-console.log(combineAges);
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+// }
 
-const combineStringAges = combine("30", "26", "as-number");
-console.log(combineStringAges);
+function printResult(num: number): undefined {
+  console.log("Result: " + num);
+  return;
+}
 
-const combineNames = combine("Max", "Anna", "as-text");
-console.log(combineNames);
+printResult(add(5, 12));
