5551ef3654f73349e8c19d6f5bd4c1e49ff9baf2
petanicode <syabilkahfialfatih@gmail.com>
2023-12-26 14:36:40 +0800

    Functions as Types

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+// function printResult(num: number): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+console.log(combineValues(8, 8));
--- app.ts
+++ app.ts
@@ -2,13 +2,20 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-// function printResult(num: number): void {
-//   console.log("Result: " + num);
-// }
-
-function printResult(num: number): undefined {
+function printResult(num: number): void {
   console.log("Result: " + num);
-  return;
 }
 
+// function printResult(num: number): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
+
 printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+
+console.log(combineValues(8, 8));
