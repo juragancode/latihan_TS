222b75b850616b709299e4f1287363821b5f91c1
petanicode <syabilkahfialfatih@gmail.com>
2023-12-26 14:49:18 +0800

    Function Types & Callbacks

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ function printResult(num) {
 //   console.log("Result: " + num);
 //   return;
 // }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
 console.log(combineValues(8, 8));
+addAndHandle(15, 30, function (result) {
+    console.log(result);
+    return result;
+});
--- app.ts
+++ app.ts
@@ -11,6 +11,11 @@ function printResult(num: number): void {
 //   return;
 // }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (a: number, b: number) => number;
@@ -19,3 +24,8 @@ combineValues = add;
 // combineValues = printResult;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(15, 30, (result) => {
+  console.log(result);
+  return result;
+});
