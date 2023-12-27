05b0c8e9ed54f02d4773e0717c162421e6dac8f8
petanicode <syabilkahfialfatih@gmail.com>
2023-12-27 14:59:18 +0800

    The never Type

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
+function generateError(message, code) {
+    throw {
+        message: message,
+        errorCode: code,
+    };
+    // while (true) {
+    //   // Infinite loop
+    // }
 }
-// function printResult(num: number): undefined {
-//   console.log("Result: " + num);
-//   return;
-// }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-console.log(combineValues(8, 8));
-addAndHandle(15, 30, function (result) {
-    console.log(result);
-    return result;
-});
+var result = generateError("An error occurred!", 500);
+console.log(result);
